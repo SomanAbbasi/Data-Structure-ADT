d5e503911a76e5adad9b92e7486114c7ab57f469
SomanAbbasi <mssabbasi306@gmail.com>
2026-03-08 05:18:59 +0500

    Daily Temp.

--- Stack/daily_temperature.cpp
+++ Stack/daily_temperature.cpp
@@ -9,12 +9,47 @@ using namespace std;
 /*
 
 739. Daily Temperatures
+
+https://leetcode.com/problems/daily-temperatures/description/
+
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        int idx = 0, count = 0;
+        stack<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (st.empty())
+            {
+                st.push(i);
+            }
+            while (!st.empty() && temperatures[st.top()] < temperatures[i])
+            {
+                res[st.top()]=i-st.top();
+                st.pop();
+            }
+            st.push(i);
+
+        }
+
+        //printRes(res);
+        return res;
+    }
+
+    void printRes(vector<int> arr)
+    {
+        for (auto num : arr)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
     }
 };
 
