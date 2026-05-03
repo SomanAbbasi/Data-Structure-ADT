418a0fd3c962e72a5f2149a5b48474da60151c99
SomanAbbasi <mssabbasi306@gmail.com>
2026-05-04 01:45:27 +0500

    adding diameter of tree

--- BinarySearchTree/heightOfBST.cpp
+++ BinarySearchTree/heightOfBST.cpp
@@ -34,6 +34,25 @@ int height(Node *root)
 }
 
 /*
+
+ // Diamteter of tree  => longest path
+
+int diameter = 0;
+
+int height(TreeNode* root) {
+    if (!root) return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    diameter = max(diameter, left + right);
+
+    return 1 + max(left, right);
+}
+*/
+
+
+/*
  level order traversal
 
 int height(Node *root)
